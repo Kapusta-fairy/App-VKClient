060f7e1473892cf4a677855ea5c7fb707db6b492
Yevgeniy Kap <kapustaevg@gmail.com>
2023-08-02 13:35:10 +0300

    add files utils

--- src/__init__.py
+++ src/__init__.py
@@ -1,19 +1,11 @@
-import os
-import sys
-from pathlib import Path
-
 from dotenv import load_dotenv
 from loguru import logger
 
 from src.config import Config
+from src.utils.files import get_kv_files_paths
 
 load_dotenv()
 
-kv_files_paths = []
-views = Path(sys.path[0]).joinpath('views')
-for root, dirs, paths in os.walk(views):
-    kv_paths = [path for path in paths if(path.endswith('.kv'))]
-    [kv_files_paths.append(f'{views}/{kv_path}') for kv_path in kv_paths]
-
+kv_files_paths = get_kv_files_paths()
 config = Config()
 logger.debug(f'init app success')
---
+++ src/utils/files.py
@@ -0,0 +1,12 @@
+import os
+import sys
+from pathlib import Path
+
+
+def get_kv_files_paths():
+    kv_files_paths = []
+    views = Path(sys.path[0]).joinpath('views')
+    for root, dirs, paths in os.walk(views):
+        kv_paths = [path for path in paths if(path.endswith('.kv'))]
+        [kv_files_paths.append(f'{views}/{kv_path}') for kv_path in kv_paths]
+    return kv_files_paths
