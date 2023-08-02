523563cec0cd3fc77cd1eac0cb60b5df2af28af5
Yevgeniy Kap <kapustaevg@gmail.com>
2023-08-02 11:55:33 +0300

    add __create_header

--- src/controllers/message.py
+++ src/controllers/message.py
@@ -6,7 +6,11 @@ from kivymd.uix.boxlayout import MDBoxLayout
 class Message(MDBoxLayout):
     def __init__(self, message, vk):
         super().__init__()
+        self.header.text = self.__create_header(message, vk)
+        self.text.text = message.get('text')
+
+    @staticmethod
+    def __create_header(message, vk):
         name = vk.get_name_by_id(message.get('from_id'))
         date = ctime(message.get('date'))[10:19]
-        self.header.text = f'{name} {date}'
-        self.text.text = message.get('text')
+        return f'{name} {date}'
